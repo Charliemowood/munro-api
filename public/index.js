10e0b86f16e9de9d13e3915ee407640b0fa43140
Charlie Wood <charliemowood@gmail.com>
2017-06-25 22:49:10 +0100

    Add basic interaction with API, CSS and print details

--- public/index.js
+++ public/index.js
@@ -1,5 +1,28 @@
-var app = function(){
+var onLoad = function(){
+  var url = 'https://munroapi.herokuapp.com/api/munros';
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.send();
 
+
+  request.addEventListener('load', function(){
+  var jsonString = request.responseText;
+  var munros = JSON.parse(jsonString);
+  var list = populateList(munros);
+  });
 }
 
-window.addEventListener('load', app);
+var populateList = function(munros) {
+  var div = document.getElementById('main');
+  munros.forEach(function(munro) {
+  var ul = document.createElement('ul');
+  ul.innerText = "Munro: " + munro.name + "\n" + "Height: " + munro.height + "\n" + "Latitude " + munro.latlng_lat + "\n" + "Longitude: " + munro.latlng_lng + "\n" + "Meaning: " + munro.meaning;
+
+  div.appendChild(ul);
+
+  });
+  //
+  // return div;
+}
+
+window.addEventListener('load', onLoad);
